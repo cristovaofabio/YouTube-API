737c4b1a014164d97c7126d59d9afa9d574305d1
Cristóvão <cristovaofabio3@gmail.com>
2021-07-02 11:22:37 -0300

    First commit

---
+++ app/src/main/java/com/tecnoapp/cristovaofabio/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tecnoapp.cristovaofabio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String GOOGLE_API_KEY="AIzaSyDbJvStc3bg_-ndZYkOYP8KIcjpEcw5tHs";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
